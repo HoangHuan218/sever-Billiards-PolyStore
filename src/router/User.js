8fc95d2874853b0be99c636357c39fa0e6e9129b
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:58:04 +0700

    DONE: config router update user

--- src/router/User.js
+++ src/router/User.js
@@ -17,7 +17,7 @@ router.get("/user", GetUser);
 router.post("/logout", logout);
 router.post("/login", signin);
 router.post('/refresh-token', refreshTokenHandler);
-// router.patch("/user/:id", updateUser);
+router.patch("/user/:id", updateUser);
 // router.delete("/user/:id", DeleteUser);
 // router.get("/user/:id", DetailUser);
 // router.patch("/user/pass/:id", UpdatePassword);
