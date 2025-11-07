327ab1db2b53c501832d96dff862bb34384a7ece
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:45:06 +0700

    DONE: config router getcart

--- src/router/cart.js
+++ src/router/cart.js
@@ -9,7 +9,7 @@ import {
 
 const route = express.Router();
 route.post("/cart/:userid", AddCart);
-// route.get("/cart/:userid", GetCart);
+route.get("/cart/:userid", GetCart);
 // route.delete("/cart/:id", DeleteCart);
 // route.patch("/cart/:id", UpdateCart);
 // route.delete("/carts/:userid", DeleteAllCart);
