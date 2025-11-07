d66571863718b72ebb050cf2af37f73cd2152ebf
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:24:46 +0700

    DONE: config delete category controller

--- src/controller/Caterory.js
+++ src/controller/Caterory.js
@@ -18,16 +18,16 @@ export const CreateCategory = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
-// export const DeleteCategory = async (req, res) => {
-//   try {
-//     await Caterory.findByIdAndDelete(req.params.id);
-//     return res.status(200).json({
-//       message: "Xóa thành công ",
-//     });
-//   } catch (error) {
-//     return res.status(500).json({ message: error.message });
-//   }
-// };
+export const DeleteCategory = async (req, res) => {
+  try {
+    await Caterory.findByIdAndDelete(req.params.id);
+    return res.status(200).json({
+      message: "Xóa thành công ",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 // export const UpdateCategory = async (req, res) => {
 //   try {
 //     const data = await Caterory.findByIdAndUpdate(req.params.id, req.body, {
