15b54a4a02ba3288abe61dad0f683ea5913fac7b
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:57:20 +0700

    DONE: config router signin

--- src/router/User.js
+++ src/router/User.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.post("/register", singup);
 router.get("/user", GetUser);
 router.post("/logout", logout);
-// router.post("/login", signin);
+router.post("/login", signin);
 // router.post('/refresh-token', refreshTokenHandler);
 // router.patch("/user/:id", updateUser);
 // router.delete("/user/:id", DeleteUser);
