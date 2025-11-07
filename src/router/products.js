3e0b7d92d55f9f45e474ac19ef64f333ae4033e7
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:55:06 +0700

    DONE: config router update product

--- src/router/products.js
+++ src/router/products.js
@@ -15,6 +15,6 @@ router.get("/products/:page", Pagination);
 router.get("/product/:id", GetProductDetails);
 router.get("/products/category/:category", GetProductsCategory);
 router.post("/products", AddProduct);
-// router.patch("/products/:id", UpdateProduct);
+router.patch("/products/:id", UpdateProduct);
 // router.delete("/products/:id", DeleteProduct);
 export default router;
