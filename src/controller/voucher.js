313200c3ebba5f3234f8ba17a37661da4674a25c
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:43:02 +0700

    DONE: config get allvoucher controller

--- src/controller/voucher.js
+++ src/controller/voucher.js
@@ -13,16 +13,16 @@ export const CreateVoucher = async (req, res) => {
     });
   }
 };
-// export const getAllVoucher = async (req, res) => {
-//   try {
-//     const data = await Voucher.find();
-//     return res.status(200).json(data);
-//   } catch (e) {
-//     return res.status(500).json({
-//       message: e.message,
-//     });
-//   }
-// };
+export const getAllVoucher = async (req, res) => {
+  try {
+    const data = await Voucher.find();
+    return res.status(200).json(data);
+  } catch (e) {
+    return res.status(500).json({
+      message: e.message,
+    });
+  }
+};
 // export const UpdateVoucher = async (req, res) => {
 //   try {
 //     const data = await Voucher.findByIdAndUpdate(req.params.id, req.body, {
