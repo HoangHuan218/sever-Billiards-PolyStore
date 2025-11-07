c5f520b027828ee247e8f6e3f859c33140756f9b
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:28:02 +0700

    DONE: config get oderbyuser controller

--- src/controller/order.js
+++ src/controller/order.js
@@ -10,17 +10,17 @@ export const GetOrder = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
-// export const GetOrderByUser = async (req, res) => {
-//   try {
-//     const data = await Order.find({ userId: req.params.userid })
-//       .populate("voucher", "discount")
-//       .populate("products.productId", "name price imageUrl");
-//     console.log(req.params.userid);
-//     return res.status(200).json(data);
-//   } catch (error) {
-//     return res.status(500).json({ message: error.message });
-//   }
-// };
+export const GetOrderByUser = async (req, res) => {
+  try {
+    const data = await Order.find({ userId: req.params.userid })
+      .populate("voucher", "discount")
+      .populate("products.productId", "name price imageUrl");
+    console.log(req.params.userid);
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 // export const AddOrder = async (req, res) => {
 //   try {
 //     const order = new Order(req.body);
