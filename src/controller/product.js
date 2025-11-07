5999b8ed1de7c58857bb7eb913d93a1f7062aade
HoangHuan218 <huanhnph31623@fpt.edu.vn>
2025-11-07 22:33:57 +0700

    DONE: config get productdeltail controller

--- src/controller/product.js
+++ src/controller/product.js
@@ -28,25 +28,25 @@ const Pagination = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
-// const GetProductDetails = async (req, res) => {
-//   try {
-//     const product = await Product.findById(req.params.id).populate(
-//       "caterori",
-//       "name",
-//     );
+const GetProductDetails = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id).populate(
+      "caterori",
+      "name",
+    );
 
-//     // Check if the product exists
-//     if (!product) {
-//       return res.status(404).json({ message: "Product not found" });
-//     }
-//     return res.status(200).json({
-//       message: `Product found with ID: ${req.params.id}`,
-//       data: product,
-//     });
-//   } catch (error) {
-//     return res.status(500).json({ message: error.message });
-//   }
-// };
+    // Check if the product exists
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    return res.status(200).json({
+      message: `Product found with ID: ${req.params.id}`,
+      data: product,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 // const GetProductsCategory = async (req, res) => {
 //   try {
 //     const { category } = req.params;
