a22be04666a61b19085ac59390e2defae8bd599a
lambthph41177-dev <huanhnph31623@fpt.edu.vn>
2025-12-07 01:17:54 +0700

    done: fix bug crash server

--- be-nodejs-main/src/router/dashboard.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import DashboardStats from "../controller/dashboard";
-
-const route = express.Router();
-
-route.get("/dashboard", DashboardStats);
-export default route;
